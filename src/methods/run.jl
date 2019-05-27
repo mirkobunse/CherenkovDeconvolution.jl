f2bab253d30a9bc4ed4fda2858addf8fd82786af
Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>
2019-05-27 17:04:14 +0200

    Updated copyright notice

--- src/methods/run.jl
+++ src/methods/run.jl
@@ -1,3 +1,24 @@
+# 
+# CherenkovDeconvolution.jl
+# Copyright 2018, 2019 Mirko Bunse
+# 
+# 
+# Deconvolution methods for Cherenkov astronomy and other use cases in experimental physics.
+# 
+# 
+# CherenkovDeconvolution.jl is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with CherenkovDeconvolution.jl.  If not, see <http://www.gnu.org/licenses/>.
+# 
 """
     run(data, train, y, x; kwargs...)
 
