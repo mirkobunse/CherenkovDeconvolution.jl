cd1ca58d3625c8fd0d7919f8930a812877c7beeb
Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>
2021-04-07 15:10:25 +0200

    Extreme programming: start by defining the goal API in the tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -51,3 +51,8 @@ include("methods/ibu.jl")
 @nbinclude "../doc/01-getting-started.ipynb"
 @nbinclude "../doc/02-inspection.ipynb"
 @nbinclude "../doc/03-adaptive-stepsize.ipynb"
+
+# check that deprecation redirections work
+@nbinclude "deprecated/01-getting-started.ipynb"
+@nbinclude "deprecated/02-inspection.ipynb"
+@nbinclude "deprecated/03-adaptive-stepsize.ipynb"
