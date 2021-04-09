520c6acff40fc0e2d7c67fbe3fc70fd02bd316e7
Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>
2021-04-09 12:19:51 +0200

    During testing, execute each jupyter notebook in its own module

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using Test, Random, Statistics
-using ScikitLearn, Distances, Discretizers, NBInclude
+using ScikitLearn, Distances, Discretizers
 using CherenkovDeconvolution
 
 Random.seed!(42) # make tests reproducible
@@ -8,12 +8,22 @@ include("DeconvUtil.jl")
 include("Binnings.jl")
 include("Methods.jl")
 
+# execute each jupyter notebook in its own module
+macro scoped_nbinclude(path::AbstractString)
+    return quote
+        @eval module $(Symbol(replace(path, r"[./\-\d]" => "")))
+            using NBInclude
+            @nbinclude $path
+        end
+    end
+end
+
 # check that no error occurs in example notebooks
-@nbinclude "../doc/01-getting-started.ipynb"
-@nbinclude "../doc/02-inspection.ipynb"
-@nbinclude "../doc/03-adaptive-stepsize.ipynb"
+@scoped_nbinclude "../doc/01-getting-started.ipynb"
+@scoped_nbinclude "../doc/02-inspection.ipynb"
+@scoped_nbinclude "../doc/03-adaptive-stepsize.ipynb"
 
 # check that deprecation redirections work
-@nbinclude "deprecated/01-getting-started.ipynb"
-@nbinclude "deprecated/02-inspection.ipynb"
-@nbinclude "deprecated/03-adaptive-stepsize.ipynb"
+@scoped_nbinclude "deprecated/01-getting-started.ipynb"
+@scoped_nbinclude "deprecated/02-inspection.ipynb"
+@scoped_nbinclude "deprecated/03-adaptive-stepsize.ipynb"
