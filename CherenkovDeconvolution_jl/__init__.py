7996048a1910323603bde8bddec71b976e47c86d
Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>
2021-11-23 21:49:54 +0100

    Wrap all methods

--- CherenkovDeconvolution_jl/__init__.py
+++ CherenkovDeconvolution_jl/__init__.py
@@ -6,8 +6,9 @@ from julia.api import Julia
 jl = Julia(compiled_modules=False)
 from julia import Main
 Main.eval(f'import Pkg; Pkg.activate("{resource_filename(__name__, "")}")')
-Main.eval('import CherenkovDeconvolution.Methods: deconvolve, DSEA, IBU, PRUN, RUN, SVD')
+Main.eval('using CherenkovDeconvolution')
 
+# CherenkovDeconvolution.Methods
 def deconvolve(m, X_obs, X_trn, y_trn):
     return Main.deconvolve(m, X_obs, X_trn, np.array(y_trn) + 1)
 def DSEA(classifier, **kwargs):
@@ -20,3 +21,21 @@ def RUN(binning, **kwargs):
     return Main.RUN(binning, **kwargs)
 def SVD(binning, **kwargs):
     return Main.SVD(binning, **kwargs)
+
+# CherenkovDeconvolution.Binnings
+def TreeBinning(num_clusters, **kwargs):
+    return Main.TreeBinning(num_clusters, **kwargs)
+def KMeansBinning(num_clusters, **kwargs):
+    return Main.KMeansBinning(num_clusters, **kwargs)
+
+# CherenkovDeconvolution.OptimizedStepsizes and CherenkovDeconvolution.Stepsizes
+def RunStepsize(binning, **kwargs):
+    return Main.RunStepsize(binning, **kwargs)
+def LsqStepsize(binning, **kwargs):
+    return Main.LsqStepsize(binning, **kwargs)
+def ConstantStepsize(alpha):
+    return Main.ConstantStepsize(alpha)
+def MulDecayStepsize(eta, alpha_0):
+    return Main.MulDecayStepsize(eta, alpha_0)
+def ExpDecayStepsize(eta, alpha_0):
+    return Main.ExpDecayStepsize(eta, alpha_0)
--- docker_tests/tests.py
+++ docker_tests/tests.py
@@ -8,6 +8,23 @@ X_trn = np.random.rand(500, 5)
 y_trn = np.random.randint(3, size=500)
 
 # deconvolve
-dsea = cd.DSEA(GaussianNB())
-f_est = cd.deconvolve(dsea, X_obs, X_trn, y_trn)
-print(f"The random deconvolution result of DSEA is {f_est}")
+pcc = cd.DSEA(GaussianNB())
+f_pcc = cd.deconvolve(pcc, X_obs, X_trn, y_trn)
+print(f"The random deconvolution result of PCC is {f_pcc}")
+
+dsea_plus = cd.DSEA(
+    GaussianNB(),
+    stepsize = cd.RunStepsize(cd.TreeBinning(10)),
+    K = 10,
+    epsilon = 1e-6
+)
+f_dsea_plus = cd.deconvolve(dsea_plus, X_obs, X_trn, y_trn)
+print(f"The random deconvolution result of DSEA+ is {f_dsea_plus}")
+
+ibu = cd.IBU(cd.TreeBinning(10))
+f_ibu = cd.deconvolve(ibu, X_obs, X_trn, y_trn)
+print(f"The random deconvolution result of IBU is {f_ibu}")
+
+run = cd.RUN(cd.KMeansBinning(10))
+f_run = cd.deconvolve(run, X_obs, X_trn, y_trn)
+print(f"The random deconvolution result of RUN is {f_run}")
